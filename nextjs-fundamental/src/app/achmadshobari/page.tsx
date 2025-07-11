b7d3da3dbe2fa4b16d6f424e78b8ac8249bef41a
achmadshobari <achmadshobari@gmail.com>
2025-07-12 06:16:25 +0700

    memperbaiki tampilan tai;wind

--- nextjs-fundamental/src/app/achmadshobari/page.tsx
+++ nextjs-fundamental/src/app/achmadshobari/page.tsx
@@ -1,9 +1,16 @@
-import styles from './achmad.module.css'
+import styles from "./achmad.module.css";
 
 export default function achmadshobari() {
   return (
-   <div className='flex items-center justify-center h-screen bg blue-500 text-white p-4'>
-    <h1>Assalamualaikum, nama saya Achmad Shobari, Salam kenal</h1>
-   </div>
+    <div
+      className={`${styles.container} min-h-screen flex items-center justify-center`}
+    >
+      <div className="w-full max-w-4xl mx-auto bg-black bg-opacity-25 rounded-2xl p-8 md:p-12 text-center">
+        <h1 className="text-white text-4xl md:text-6xl font-black tracking-tight mb-4">
+          Achmad Shobari
+        </h1>
+        <h1 className="w-24 bg-white h-5 mx-auto my-auto rounded-2xl"></h1>
+      </div>
+    </div>
   );
 }
