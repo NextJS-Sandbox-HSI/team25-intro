e277a5d4a72ce4731560c8d7b8323d188223fbae
kemalrizky <kemalrizkyf.18@gmail.com>
2025-08-02 11:27:34 +0700

    feat: add ProjectCard component, displayed in  Projects page

--- nextjs-fundamental/src/app/kemalrizky/projects/page.tsx
+++ nextjs-fundamental/src/app/kemalrizky/projects/page.tsx
@@ -1,28 +1,35 @@
 import styles from '../kemalrizky.module.css'
+import ProjectCard from '../_components/ProjectCard';
 
 export default function KemalRizky() {
     return(
         <div className={styles.page}>
-      <div className={styles.card}>
-        <h1 className={styles.name}>Projects</h1>
-        <hr className={styles.divider} />
-        <p className={styles.bio}>
-          Hi, I'm <span className={styles.bold}>Kemal Rizky Fadhlurrohman</span> , currently living in Jakarta. My previous work involved building Internet of Things (IoT)
-          products and managing research projects.
-        </p>
-        <p className={styles.bio}>
-          I'm passionate about building impactful technologies and I'm looking forward to explore the realm of web development.
-        </p>
-        <div className={styles.keywords}>
-          <span>🤖 IoT</span>
-          <span>🍎 Prod Dev</span>
-          <span>🧰 Project Management</span>
+          <div className="w-full max-w-4xl mx-auto grid grid-cols-1 md:grid-cols-2 gap-8">
+            <ProjectCard
+            title="Waterbox"
+            description="IoT device for water monitoring"
+            href="https://github.com/kemalrizky/waterbox-dev"
+            keywords={["Arduino", "C++", "IoT"]}
+            />
+            <ProjectCard
+            title="Broiler Mass Prediction"
+            description="ML model to calculate broiler average mass from video footage"
+            href="https://github.com/kemalrizky/broiler-mass-predict"
+            keywords={["Python", "Tensorflow", "Data Science"]}
+            />
+            <ProjectCard
+            title="Pendulum Period Detection"
+            description="Program to detect pendulum period from an image data using OpenCV"
+            href="https://github.com/kemalrizky/pendulum_period_detection"
+            keywords={["OpenCV", "Python", "Physical Modelling"]}
+            />
+            <ProjectCard
+            title="Coupled Ball Simulation"
+            description="Program for simulating a movement of coupled balls based on physical constrains"
+            href="https://github.com/kemalrizky/coupled_ball"
+            keywords={["Python", "Physical Modelling"]}
+            />
+          </div>
         </div>
-        <div className={styles.links}>
-          <a href="https://linkedin.com/in/kemalrizky" target="_blank">LinkedIn</a>
-          <a href="https://github.com/kemalrizky" target="_blank">GitHub</a>
-        </div>
-      </div>
-    </div>
     );  
 } 
